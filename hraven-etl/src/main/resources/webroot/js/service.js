5a1367ed2f6840f3043881801752d1da89807107
Chia Chuan Chuang <ChiaChuanChuang@itri.org.tw>
2014-11-23 21:07:26 +0800

    add tabs and option UI

--- hraven-etl/src/main/resources/webroot/js/service.js
+++ hraven-etl/src/main/resources/webroot/js/service.js
@@ -1,278 +1,148 @@
 $(document).ready(function() {
-  
-function timeConverter(UNIX_timestamp){
-  var a = new Date(UNIX_timestamp);
-  var months = ['01','02','03','04','05','06','07','08','09','10','11','12'];
-  var year = a.getFullYear();
-  var month = months[a.getMonth()];
-  var date = a.getDate();
-  var hour = a.getHours();
-  var min = a.getMinutes();
-  var sec = a.getSeconds();
-  var time = ''+year+'/'+month+'/'+date+'/'+hour+':'+min;
-  return time;
-}
-  
-   $.getJSON("http://192.168.56.201:8080/api/v1/job/NCHC/hdadm", function(json){
-		var jobName = [];
-		var runTime = [];
-		var cost = [];
-		var megabyteMillis = [];
-		
-		
-        for(var k in json) {
-			jobName[k] = json[k]['jobName'];
-			runTime[k] =json[k]['runTime'];
-			cost[k]=json[k]['cost'];
-			megabyteMillis[k]=json[k]['megabyteMillis'];
-		}
-		
-		var chart = new Highcharts.Chart({
+    // UI
+    $("#main").tabs();
+    $("#startdatetimepicker1").datetimepicker().button();
+    $("#enddatetimepicker").datetimepicker().button();
+    $("#typeToggle").buttonset();
+
+    $("#jobuser").button();
+    $("#qb").button();
+    $("#job").button();
+    $("#jobst").datetimepicker().button();
+    $("#jobet").datetimepicker().button();
+    $("#typeToggle2").buttonset();
+
+    function timeConverter(UNIX_timestamp){
+      var a = new Date(UNIX_timestamp);
+      var months = ['01','02','03','04','05','06','07','08','09','10','11','12'];
+      var year = a.getFullYear();
+      var month = months[a.getMonth()];
+      var date = a.getDate();
+      var hour = a.getHours();
+      var min = a.getMinutes();
+      //var sec = a.getSeconds();
+      var time = ''+year+'/'+month+'/'+date+'/'+hour+':'+min;
+      return time;
+    }
+
+    function convertToTS(HumanTime){
+        var d = HumanTime.match(/\d+/g); // extract date parts
+        return +new Date(d[0], d[1] - 1, d[2], d[3], d[4], d[5]); // build Date object
+    }
+
+    function queryUser(user, start_ts,end_ts){
+        alert(user + start_ts + end_ts);
+        //var ts = convertToTS("2014/11/22 12:59:00");
+        //console.log(ts);
+        var para = {
+            xtitle: "Job Name",
+            ytitle: "COST (NTD)",
+            series: "COST",
+            tip: " NTD."
+        };
+        var items = [["JobA","JobB","JobC"],[100,200,300],[1,2,3],[0.1,0.2,0.3]];
+        plot(items[0],items[1],para);
+        return items;
+
+        //TODO: return 2d array
+        //['X-axis'][...]
+        //['cost']  [...]
+        //['time']  [...]
+        //['MbMs']  [...]
+    }
+
+    function queryJob(user,job,start_ts,end_ts){
+        alert(user + job + start_ts + end_ts);
+        //TODO: return 2d array
+        //['X-axis'][...]
+        //['cost']  [...]
+        //['time']  [...]
+        //['MbMs']  [...]
+    }
+
+    function plot(Xaxis, Yaxis, showPara){
+        var chart = new Highcharts.Chart({
             chart: {
                 renderTo: 'container1',
                 defaultSeriesType: 'line',
                 margin: [50, 150, 60, 80]
             },
-                              
-                               
+
             xAxis: {
-                categories: jobName,
+                categories: Xaxis,
                 title: {
-                    text: 'Job Name'
+                    text: showPara.xtitle
                 }
             },
             yAxis: {
                 title: {
-                    text: 'COST (NTD)'
+                    text: showPara.ytitle
                 },
-				plotLines: [{
+                plotLines: [{
                     value: 0,
                     width: 1,
                     color: '#808080'
                 }],
-				min: 0
-				
+                min: 0
             },
             tooltip: {
                 formatter: function() {
-					return this.x +'<br/>'+ this.y+' NTD.';
-                    }
-                },
-                               
-            series: [{
-                name: 'COST',
-                data: cost,
-				color: '#FF0000'
-            }]
-        });	
-		
-		
+                    return this.x +'<br/>'+ this.y+ showPara.tip;
+                }
+            },
 
-		
-		var chart = new Highcharts.Chart({
-            chart: {
-                renderTo: 'container2',
-                defaultSeriesType: 'line',
-                margin: [50, 150, 60, 80]
-            },
-            xAxis: {
-                categories: jobName,
-                title: {
-                    text: 'Job Name'
-                }
-            },
-            yAxis: {
-                title: {
-                    text: 'Time (ms)'
-                },
-				plotLines: [{
-                    value: 0,
-                    width: 1,
-                    color: '#808080'
-                }]
-            },
-            tooltip: {
-                formatter: function() {
-					return this.x +'<br/>'+ this.y/1000 +' sec.';
-                    }
-                },
-                               
             series: [{
-                name: 'runTime',
-                data: runTime,
-				color: '#00FF00'
+                name: showPara.series,
+                data: Yaxis,
+                color: '#FF0000'
             }]
-        });		
-		
-		var chart = new Highcharts.Chart({
-            chart: {
-                renderTo: 'container3',
-                defaultSeriesType: 'line',
-                margin: [50, 150, 60, 80]
-            },
-                              
-                               
-            xAxis: {
-                categories: jobName,
-                title: {
-                    text: 'Job Name'
-                }
-            },
-            yAxis: {
-                title: {
-                    text: 'RAM X TIME <br> megabyteMillis)'
-                },
-				plotLines: [{
-                    value: 0,
-                    width: 1,
-                    color: '#808080'
-                }]
-            },
-            tooltip: {
-                formatter: function() {
-					return this.x +'<br/>'+ this.y/1000 +' MBSec.';
-                    }
-                },
-                               
-            series: [{
-                name: 'megabyteMillis',
-                data: megabyteMillis,
-				color: '#0000FF'
-            }]
-        });	
+        });
+    }
+
+
+    var dataarray;
+    $("#user").button();
+    $("#qqq").button().click(function(){
+        var v1 = $("#user").val();
+        var v2 = $("#startdatetimepicker1").val();
+        var v3 = $("#enddatetimepicker").val();
+        dataarray = queryUser(v1,v2,v3);
     });
 
-	
+    $("#btncost").click(function () {
+            var para = {
+                xtitle: "Job Name",
+                ytitle: "COST (NTD)",
+                series: "COST",
+                tip: " NTD."
+            };
+            plot(dataarray[0],dataarray[1],para);
+        }
+    );
+
+    $("#btntime").click(function () {
+            var para = {
+                xtitle: "Job Name",
+                ytitle: "Time (ms)",
+                series: "runTime",
+                tip: " sec."
+            };
+            plot(dataarray[0],dataarray[2],para);
+        }
+    );
+
+    $("#btnmbms").click(function () {
+            var para = {
+                xtitle: "Job Name",
+                ytitle: "RAM X TIME <br> (megabyteMillis)",
+                series: "megabyteMillis",
+                tip: " MBSec."
+            };
+            plot(dataarray[0],dataarray[3],para);
+        }
+    );
 
 
-  	$.getJSON("http://192.168.56.201:8080/api/v1/job/NCHC/hdadm/TeraGen", function(json){
-		var submitDate = [];
-		var runTime = [];
-		var megabyteMillis =[];
-		var cost =[];
-		for(var k in json) {
-			
-			var ts = json[k]['submitDate'];
-			var ts2 = timeConverter(ts);			
-			submitDate[k] = ts2;
-			runTime[k] = json[k]['runTime'];
-			megabyteMillis[k] = json[k]['megabyteMillis'];	
-			cost[k] = json[k]['cost'];
-		}
 
-		
-		var chart = new Highcharts.Chart({
-            chart: {
-                renderTo: 'container4',
-                defaultSeriesType: 'line',
-                margin: [50, 150, 60, 80]
-            },
-                              
-                               
-            xAxis: {
-                categories: submitDate,
-                title: {
-                    text: 'submit Time'
-                }
-            },
-            yAxis: {
-                title: {
-                    text: 'COST (NTD)'
-                },
-				plotLines: [{
-                    value: 0,
-                    width: 1,
-                    color: '#808080'
-                }],
-				min: 0
-				
-            },
-            tooltip: {
-                formatter: function() {
-					return this.x +'<br/>'+ this.y+' NTD.';
-                    }
-                },
-                               
-            series: [{
-                name: 'COST',
-                data: cost,
-				color: '#FF0000'
-            }]
-        });	
-		
-		
-		var chart = new Highcharts.Chart({
-            chart: {
-                renderTo: 'container4',
-                defaultSeriesType: 'line',
-                margin: [50, 150, 60, 80]
-            },
-            xAxis: {
-                categories: submitDate,
-                title: {
-                    text: 'submit Time'
-                }
-            },
-            yAxis: {
-                title: {
-                    text: 'Time (ms)'
-                },
-				plotLines: [{
-                    value: 0,
-                    width: 1,
-                    color: '#808080'
-                }]
-            },
-            tooltip: {
-                formatter: function() {
-					return this.x +'<br/>'+ this.y/1000 +' sec.';
-                    }
-                },
-                               
-            series: [{
-                name: 'runTime',
-                data: runTime,
-				color: '#00FF00'
-            }]
-        });		
-		
-		
-		var chart = new Highcharts.Chart({
-            chart: {
-                renderTo: 'container6',
-                defaultSeriesType: 'line',
-                margin: [50, 150, 60, 80]
-            },
-                              
-                               
-            xAxis: {
-                categories: submitDate,
-                title: {
-                    text: 'submit time'
-                }
-            },
-            yAxis: {
-                title: {
-                    text: 'RAM X TIME <br> megabyteMillis)'
-                },
-				plotLines: [{
-                    value: 0,
-                    width: 1,
-                    color: '#808080'
-                }]
-            },
-            tooltip: {
-                formatter: function() {
-					return this.x +'<br/>'+ this.y/1000 +' MBSec.';
-                    }
-                },
-                               
-            series: [{
-                name: 'megabyteMillis',
-                data: megabyteMillis,
-				color: '#0000FF'
-            }]
-        });	
-		
-	});
+
 });
